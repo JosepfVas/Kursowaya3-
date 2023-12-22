ba635cba2237150ee696701ba02e0c76d5b4b19b
Josef Vas <yovzhi@icloud.com>
2023-12-22 11:47:33 +0100

    adding filter func

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,12 @@ def open_json_file(filename):
     ROOT_DIR = os.path.dirname(__file__)
     file_path = os.path.join(ROOT_DIR, filename)
 
-    with open(file_path, encoding="utf-8") as file:
+    with open(file_path, 'r', encoding="utf-8") as file:
         data = json.load(file)
     return data
 
+
+def get_executed(list_of_dicts):
+    executed_list = [item for item in list_of_dicts if any(value == 'EXECUTED' for value in item.values())]
+    return executed_list
 

76e683b551827390500313472f3ce0366861a826
Josef Vas <yovzhi@icloud.com>
2023-12-22 11:19:08 +0100

    adding open_file func

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,12 @@
+import os
+import json
+
+def open_json_file(filename):
+    ROOT_DIR = os.path.dirname(__file__)
+    file_path = os.path.join(ROOT_DIR, filename)
+
+    with open(file_path, encoding="utf-8") as file:
+        data = json.load(file)
+    return data
+
 

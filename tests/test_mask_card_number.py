45f9e3e4b3815a0f5336077c0de4877d782170fa
Josef Vas <yovzhi@icloud.com>
2023-12-28 13:28:37 +0100

    fix some tests

--- tests/test_mask_card_number.py
+++ tests/test_mask_card_number.py
@@ -1,11 +1,12 @@
 from src.utils import mask_card_number
 
+
 def test_mask_card_number_credit_card():
-    input_number = '1234 5678 9012 3456'
+    input_number = 'Maestro 1308795367077170'
     result = mask_card_number(input_number)
-    assert result == '1234 56** **** 3456'
+    assert result == 'Maestro 1308 79** **** 7170'
 
 def test_mask_card_number_bank_account():
-    input_number = 'счет 1234 567890123456'
+    input_number = 'Счет 71687416928274675290'
     result = mask_card_number(input_number)
-    assert result == 'счет **3456'
+    assert result == 'Счет **5290'
